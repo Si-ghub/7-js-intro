79f77d2f54d4ad2e2a1a18dc35ad87da7fa0011d
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-05-04 18:57:25 +0300

    Intro

---
+++ js/00-intro.js
@@ -0,0 +1,22 @@
+
+// single-line comment
+
+/* multi 
+line 
+comment
+*/
+
+/*
+
+KONCEPTAI:
+1) kintamieji - isimena informacija
+2) salygos sakiniai - tarpusavio palyginimas
+3) ciklai - darbas su pasikartojancio formato informacija, 
+pasikartojanciu procesu valdymas
+4) funkcijos - uzdara logika, kuria galima perpanaudoti pagal poreiki
+5) ivykiai - uzsikrove puslapis, paspaude mygtuka, paspaude peles mygtuka,
+pradejo siusti faila, issiunte uzklausa ir t.t g.b daug bet pgr apie 8
+6) oop - objektinis programavimas tai tik skonio reikalas 
+
+
+*/
