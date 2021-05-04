00b4c82336a0db2072d1ac7c87f487cc69e096a4
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-05-04 20:08:33 +0300

    Const

---
+++ js/02-num.js
@@ -0,0 +1,29 @@
+/*
+NUMBERS
+teigiami, nulis, neigiami
+teigiami, neigiami
+desimtainiai, sveikieji
+tikri skaiciai, begalybes (teigiamos neigiamos), NaN
+
+const teigiamas = 5;
+const neigiamas = -8;
+const nulis = 0;
+const nulisMinus = -0;
+
+console.log(nulis, nulisMinus);
+
+const begalybe = Infinity;
+const begalybeMinus = -Infinity;
+console.log(begalybe, begalybeMinus);
+
+const nesamone = NaN;
+console.log(nesamone);
+
+const puseZodzio = 'labas' / 2;
+console.log(puseZodzio);
+
+
+
+
+
+*/
