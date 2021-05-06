a41c32668b801417d5880933bc7ac12d514bff94
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-05-06 21:10:36 +0300

    Day6

---
+++ js/07.revers.js
@@ -0,0 +1,6 @@
+/*
+Duotas array su tekstinemis reiksmemis (string)
+Reikia: sijungti juos atbulai iterpiant ", "
+*/
+// labas, rytas, tau, sakau
+const dictionary = ['sakau', 'tau', 'rytas' 'labas'];
