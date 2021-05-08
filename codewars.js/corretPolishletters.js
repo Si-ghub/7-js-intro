1ca5e0bb9d342209aa57e32b73b9debc4747aa1d
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-05-08 19:51:06 +0300

    #0508

---
+++ codewars.js/corretPolishletters.js
@@ -0,0 +1,65 @@
+/*
+ą -> a,
+ć -> c,
+ę -> e,
+ł -> l,
+ń -> n,
+ó -> o,
+ś -> s,
+ź -> z,
+ż -> z
+*/
+
+
+function correctPolishLetters (string) {
+   let convertedString = '';
+
+   for( let i=0; i<string.length; i++)
+   const raide = string[i];
+
+   swith (raide) {
+       case 'ą':
+           convertedString += a
+           break;
+        case 'ć':
+            convertedString += c
+            break;
+        case 'ę':
+           convertedString += e
+           break;
+        case 'ł':
+           convertedString += l
+           break;
+        case 'ń':
+           convertedString += n
+           break;
+        case 'ó':
+           convertedString += o
+           break;
+        case 'ś':
+           convertedString += s
+           break;
+        case 'ź':
+            convertedString += z
+            break;
+        case 'ż':
+           convertedString += z
+           break;
+
+default:
+    convertedString += 
+   
+
+
+
+
+   return convertedString;
+  }
+
+
+  console.log(correctPolishLetters("Jędrzej Błądziński"),"Jedrzej Bladzinski");
+  console.log(correctPolishLetters("Jędrzej Błądziński"),"Jedrzej Bladzinski");
+  console.log(correctPolishLetters("Lech Wałęsa"),"Lech Walesa");
+  console.log(correctPolishLetters("Maria Skłodowska-Curie"),"Maria Sklodowska-Curie");
+  
+  
